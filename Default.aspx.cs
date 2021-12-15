630d94a56e496486ee89c76fc79eee55d54ad774
unknown <sergio.luis.huanca@gmail.com>
2021-12-15 07:18:32 -0400

    primera version

---
+++ Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int bas = Convert.ToInt32(TextBox1.Text);
+        int pot = Convert.ToInt32(TextBox2.Text);
+        int res = 1;
+        ClassLibrary1.Class1 c = new ClassLibrary1.Class1();
+        res = c.potencia(bas, pot);
+        TextBox3.Text = res.ToString();
+    }
+}
